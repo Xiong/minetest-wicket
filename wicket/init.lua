a6dbcb24bc2ec2b82f11587ba06f03bc4815add6
maskedlua <maskedlua@copyfree.org>
2013-12-26 23:19:12 -0600

    Starting on init.lua (minetest module init) (changing author correclty) Signed-off-by: maskedlua <maskedlua@copyfree.org>

--- wicket/init.lua
+++ wicket/init.lua
@@ -1,1 +1,12 @@
---empty file
+
+minetest.register_on_newplayer(
+    function(player)
+        local pw = tostring(math.random(1, 999999))
+        local uname = player:get_player_name()
+        local msg = "Your new password is " .. pw
+        if validate(uname, pw)
+            minetest.chat_send_player(player:get_player_name(), msg)
+    end)
+
+function validate(uname, pw)
+end
